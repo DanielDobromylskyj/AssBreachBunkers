9df70121899baa7f10a9be180c67cec2aa7c72e0
DanielDobromylskyj <90842024+DanielDobromylskyj@users.noreply.github.com>
2026-09-14 16:07:18 +0100

    added arsenal fetch API

--- web_sever.py
+++ web_sever.py
@@ -1,5 +1,6 @@
-from flask import Flask, redirect, resp, request, url_for, send_from_directory, send_file
+from flask import Flask, redirect, request, url_for, send_from_directory, send_file
 from flask_login import LoginManager, UserMixin, current_user, login_required, login_user, logout_user
+import json
 
 app = Flask(__name__)
 app.secret_key = "dev-only-change-me"
@@ -9,7 +10,28 @@ login_manager.login_view = "login"
 
 users = {}  # Pure skill, We don't need no DB for logins
 
+global blasters
+blasters = {}
+
 DEFAULT_POINTS = 10
+
+def reset():
+    global blasters
+    blasters = {}
+
+    with open("private/arsenal.json", "r") as f:
+        arsenal = json.load(f)
+
+    for side in arsenal["blasters"].keys():
+        if side in blasters:
+            raise LookupError("Why tf does that already exist?!?!")
+
+        blasters[side] = []
+
+        for blaster in arsenal["blasters"][side]:
+            blaster["available"] = blaster['max_available']
+            blasters[side].append(blaster)
+
 
 class User(UserMixin):
     def __init__(self, username):
@@ -57,6 +79,15 @@ def landing():
 def dashboard():
     return send_file("public/html/dashboard.html", "text/html")
 
+@app.route("/arsenal/<string:side>")
+@login_required
+def get_arsenal(side):
+    if side not in ("breach", "bunker"):
+        return {"error": "Invalid side. Must be 'breach' or 'bunker'"}
+
+    return blasters[side]
+
+
 
 # Everything under /public/* is intentionally unauthenticated.
 @app.route("/public/<path:path>")
@@ -65,5 +96,7 @@ def public(path):
 
 
 if __name__ == "__main__":
+    reset()  # Load all the guns and stuff
+
     app.run(debug=True)
 
